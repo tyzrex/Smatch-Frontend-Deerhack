d861c773c05673b6c488d042f66249efc878e827
Sulav Baral <sulavbaral58@gmail.com>
2024-05-11 12:43:53 +0545

    form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -120,7 +120,7 @@ export function LoginForm() {
         </Button>
       </form>
       <div className="mt-4 text-center text-sm">
-        Don't have an account?
+        Don&apos;t have an account?
         <Link className="underline" href="#">
           Sign up
         </Link>
